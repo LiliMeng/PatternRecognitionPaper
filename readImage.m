8cdc7348147c87568ca17300068d34166ebd5ce4
LiliMeng <lilimeng1103@gmail.com>
2016-12-13 15:37:24 -0800

    Update readImage.m

--- readImage.m
+++ readImage.m
@@ -3,26 +3,34 @@ close all;
 
 [tree, RootName, DOMnode] = xml_read('/home/lci/PR2017/matlabCode/xmlIOTools/00000000.xml')
  
-pts_2d_char=tree.pts_2d.data;
- 
-pts_2d_double_tmp=strread(pts_2d_char);
+pts_2d_char=strsplit(tree.pts_2d.data);
+
+for i=1:length(pts_2d_char)
+    pts_2d_double_tmp(i)=str2num(cell2mat(pts_2d_char(i)));
+end
 
 pts_2d_double=reshape(pts_2d_double_tmp,[],2);
 
 
 
-pts_3d_char=tree.pts_3d.data;
+pts_3d_char=strsplit(tree.pts_3d.data);
 
-pts_3d_double_tmp=strread(pts_3d_char);
+for i=1:length(pts_3d_char)
+    pts_3d_double_tmp(i)=str2num(cell2mat(pts_3d_char(i)));
+end
+
 
 pts_3d_double=reshape(pts_3d_double_tmp,[],3);
 
 
-fid1 = fopen('/home/lci/PR2017/matlabCode/pts2d.txt', 'w');
+
 
 [N_pts2d, d_pts2d]=size(pts_2d_double);
 [N_pts3d, d_pts3d]=size(pts_3d_double);
 
+assert(N_pts2d==N_pts3d);
+
+fid1 = fopen('/home/lci/PR2017/matlabCode/pts2d.txt', 'w');
 for i=1:N_pts2d
     fprintf(fid1, '%d %d\n', pts_2d_double(i,1), pts_2d_double(i,2));
 end
